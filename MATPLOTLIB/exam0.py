7d2e7d0e4b7a6184a4c97fed2a774f01cd1bfc2f
fg0d <35772301+techfantasm@users.noreply.github.com>
2019-06-27 11:45:17 +0800

    Exam0

---
+++ MATPLOTLIB/exam0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3 #Python3 дээр бичигдсэн гэдгийг илэрхийлнэ.
+# -*- coding: utf-8 -*-
+"""
+. install numpy ( sudo apt install python-numpy OR sudo pip install numpy)
+1. install matplotlib (sudo apt install python-matplotlib OR sudo pip install matplotlib)
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+C,S = np.cos(X), np.sin(X)
+
+plt.plot(X,C)
+plt.plot(X,S)
+
+plt.show()
