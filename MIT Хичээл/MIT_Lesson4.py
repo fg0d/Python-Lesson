50c3142ae79f22c3eca49c716aa0ed35e5397c4c
fg0d <35772301+techfantasm@users.noreply.github.com>
2019-06-27 12:06:23 +0800

    MIT Хичээл #4

---
+++ MIT Хичээл/MIT_Lesson4.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+#################
+## EXAMPLE: simple Цэг class
+#################
+
+class Цэг(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return "<" + str(self.x) + "," + str(self.y) + ">"
+    def зай(self, өөр):
+        x_diff_sq = (self.x-өөр.x)**2
+        y_diff_sq = (self.y-өөр.y)**2
+        return (x_diff_sq + y_diff_sq)**0.5
+
+а = Цэг(3,4)
+б = Цэг(0,0)
+а.x=4
+print(а)
+print(б)
+print(а.зай(б), " ")
